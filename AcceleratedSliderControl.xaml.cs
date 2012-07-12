53a798b498eb5b21481c995eebf1927c6660d315
Gyuris Krisztian <gyurisc@gmail.com>
2012-07-12 06:46:26 +0200

    adding sliders to the mainpage

---
+++ AcceleratedSliderControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace AcceleratedKnob_WP7
+{
+	public partial class AcceleratedSliderControl : UserControl
+	{
+		public AcceleratedSliderControl()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -16,44 +16,10 @@ namespace AcceleratedKnob_WP7
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        Accelerometer accelerometer = new Accelerometer();
-
-        // rotations    
-        public double darkImageRotation = 0;
-        public double leftImageRotation = 0;
-        public double rightImageRotation = 0; 
-
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
-        {
-            accelerometer = new Accelerometer();
-
-            accelerometer.CurrentValueChanged += new EventHandler<SensorReadingEventArgs<AccelerometerReading>>(acc_CurrentValueChanged);
-            accelerometer.Start();
-        }
-
-        void acc_CurrentValueChanged(object sender, SensorReadingEventArgs<AccelerometerReading> e)
-        {
-            Deployment.Current.Dispatcher.BeginInvoke(() => updateMyScreen(e.SensorReading));
-        }
-
-        void updateMyScreen(AccelerometerReading e)
-        {
-            Microsoft.Xna.Framework.Vector3 v = e.Acceleration;
-
-            darkImageRotation = (darkImageRotation * 0.7) + v.X * 0.3; 
-            leftImageRotation = (leftImageRotation * 0.7) + (v.Y - darkImageRotation) * 0.3; 
-            rightImageRotation = (rightImageRotation * 0.7) + (v.Z - leftImageRotation) * 0.3;
-
-            
-            DarkImageProjection.RotationZ = darkImageRotation * 100;
-            RightImageProjection.RotationZ = rightImageRotation * 100;
-            LeftImageProjection.RotationZ = leftImageRotation * 100;
         }
 
     }
